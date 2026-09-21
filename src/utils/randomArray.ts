b6cddaa547403affa750712770dcbd323148c035
ByiDray <bravo.ordaz.adriel@gmail.com>
2026-09-21 08:56:39 -0600

    fix: min and max with default values

--- src/utils/randomArray.ts
+++ src/utils/randomArray.ts
@@ -1,8 +1,8 @@
-function generateRandomArray(size: number, min?: number, max?: number): number[]{
+function generateRandomArray(size: number, min: number  = 0, max: number = 100): number[]{
     const arr: number[] = [];
 
     for (let i = 0; i < size; i++){
-        const randomNum = Math.floor(Math.random() * (max! - min! + 1)) + min!;
+        const randomNum = Math.floor(Math.random() * (max - min + 1)) + min!;
         arr.push(randomNum);
     }
     return arr;
