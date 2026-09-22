37acbf37772890a49a541f06cc97fe54e98b2838
ByiDray <bravo.ordaz.adriel@gmail.com>
2026-09-21 21:02:11 -0600

    fix: add value validations and export the function

--- src/utils/randomArray.ts
+++ src/utils/randomArray.ts
@@ -1,4 +1,7 @@
-function generateRandomArray(size: number, min: number  = 0, max: number = 100): number[]{
+export function generateRandomArray(size: number, min: number  = 0, max: number = 100): number[]{
+    if (size <= 0) { throw new Error("El tamaño del arreglo no puede ser menor a 0"); }
+    if (min >= max) { throw new Error("El valor mínimo debe ser menor que el valor máximo."); }
+
     const arr: number[] = [];
 
     for (let i = 0; i < size; i++){
