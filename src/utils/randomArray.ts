08aff2249e1884996e089fb65cc321fa56bedc43
ByiDray <bravo.ordaz.adriel@gmail.com>
2026-09-21 21:17:52 -0600

    fix: remove exclamation mark

--- src/utils/randomArray.ts
+++ src/utils/randomArray.ts
@@ -5,7 +5,7 @@ export function generateRandomArray(size: number, min: number  = 0, max: number 
     const arr: number[] = [];
 
     for (let i = 0; i < size; i++){
-        const randomNum = Math.floor(Math.random() * (max - min + 1)) + min!;
+        const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
         arr.push(randomNum);
     }
     return arr;
