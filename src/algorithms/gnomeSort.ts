366e47a9dc6387acf5d54780b5319e72d639d010
Andy-Plaza <andy.plaza8601@alumnos.udg.mx>
2026-09-21 19:42:31 -0600

    fix: changed format and added missing semicolon

--- src/algorithms/gnomeSort.ts
+++ src/algorithms/gnomeSort.ts
@@ -3,23 +3,26 @@ import type { SortAlgorithm, SortStep } from "./types";
 function* gnomeSortSteps(arr: number[]): Generator<SortStep> {
     const a = [...arr];
     let i = 0;
+
     while (i < a.length) {
-        if (i == 0) {
-            i += 1;
+        if (i === 0) {
+            i++;
         } else {
             yield { kind: "compare", indices: [i - 1, i] };
             if (a[i] >= a[i - 1]) {
-                i += 1;
+                i++;
             } else {
                 [a[i], a[i - 1]] = [a[i - 1], a[i]];
                 yield { kind: "swap", indices: [i - 1, i] };
-                i -= 1;
+                i--;
             }
         }
     }
+
     for (let j = 0; j < a.length; j++) {
         yield { kind: "sorted", index: j };
     }
+
     yield { kind: "done" };
 }
 
@@ -27,4 +30,4 @@ export const gnomeSort: SortAlgorithm = {
     id: "gnome",
     name: "Gnome Sort",
     run: gnomeSortSteps,
-}
+};
