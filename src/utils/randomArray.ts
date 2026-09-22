a4da87d7b2e750d435e09f6030551308d282e76f
Adriel Ordaz <bravo.ordaz.adriel@gmail.com>
2026-09-21 21:38:02 -0600

    feat: added random array generator function
    * feat: added random array generator
    
    * fix: min and max with default values
    
    * fix: add value validations and export the function
    
    * fix: remove exclamation mark

---
+++ src/utils/randomArray.ts
@@ -0,0 +1,12 @@
+export function generateRandomArray(size: number, min: number  = 0, max: number = 100): number[]{
+    if (size <= 0) { throw new Error("El tamaño del arreglo no puede ser menor a 0"); }
+    if (min >= max) { throw new Error("El valor mínimo debe ser menor que el valor máximo."); }
+
+    const arr: number[] = [];
+
+    for (let i = 0; i < size; i++){
+        const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+        arr.push(randomNum);
+    }
+    return arr;
+}
