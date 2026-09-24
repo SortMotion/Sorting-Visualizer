4b967085c410fd0245bbbad2f27cdf5590ce9135
Diego Hernandez <diego.hernandez8666@alumnos.udg.mx>
2026-09-24 12:57:54 -0600

    feat: bubble sort y actualizacion gnome y merge

--- src/algorithms/gnomeSort.ts
+++ src/algorithms/gnomeSort.ts
@@ -29,5 +29,6 @@ function* gnomeSortSteps(arr: number[]): Generator<SortStep> {
 export const gnomeSort: SortAlgorithm = {
     id: "gnome",
     name: "Gnome Sort",
+    complexity: "O(n²)",
     run: gnomeSortSteps,
 };
